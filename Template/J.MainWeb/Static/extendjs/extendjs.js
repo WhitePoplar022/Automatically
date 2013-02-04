687765513462804988e061f7cc57d99b66934224
sorex <sorex1984@gmail.com>
2013-02-04 16:42:15 +0800

    为jquery 1.9添加了browser和browser.version方法

--- Template/J.MainWeb/Static/extendjs/extendjs.js
+++ Template/J.MainWeb/Static/extendjs/extendjs.js
@@ -2,7 +2,7 @@
  * 模    块	：	通用js扩展类
  * 作    者	：	Jasper
  * 编写时间	：	2012-10-22
- * 实现功能	：	添加通用方法，扩展String对象，Array对象
+ * 实现功能	：	添加通用方法，扩展String对象，Array对象，jquery在1.9版本中删除的browser和browser.version方法
  * 详细描述	：	
  * *********************************************************/
 
@@ -114,3 +114,75 @@ String.prototype.decodeURI = function ()
 {
 	return unescape(this)
 }
+
+(function ()
+{
+
+	var matched, browser;
+
+	// Use of jQuery.browser is frowned upon.
+	// More details: http://api.jquery.com/jQuery.browser
+	// jQuery.uaMatch maintained for back-compat
+	jQuery.uaMatch = function (ua)
+	{
+		ua = ua.toLowerCase();
+
+		var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
+			/(webkit)[ \/]([\w.]+)/.exec(ua) ||
+			/(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
+			/(msie) ([\w.]+)/.exec(ua) ||
+			ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) ||
+			[];
+
+		return {
+			browser: match[1] || "",
+			version: match[2] || "0"
+		};
+	};
+
+	matched = jQuery.uaMatch(navigator.userAgent);
+	browser = {};
+
+	if (matched.browser)
+	{
+		browser[matched.browser] = true;
+		browser.version = matched.version;
+	}
+
+	// Chrome is Webkit, but Webkit is also Safari.
+	if (browser.chrome)
+	{
+		browser.webkit = true;
+	} else if (browser.webkit)
+	{
+		browser.safari = true;
+	}
+
+	jQuery.browser = browser;
+
+	jQuery.sub = function ()
+	{
+		function jQuerySub(selector, context)
+		{
+			return new jQuerySub.fn.init(selector, context);
+		}
+		jQuery.extend(true, jQuerySub, this);
+		jQuerySub.superclass = this;
+		jQuerySub.fn = jQuerySub.prototype = this();
+		jQuerySub.fn.constructor = jQuerySub;
+		jQuerySub.sub = this.sub;
+		jQuerySub.fn.init = function init(selector, context)
+		{
+			if (context && context instanceof jQuery && !(context instanceof jQuerySub))
+			{
+				context = jQuerySub(context);
+			}
+
+			return jQuery.fn.init.call(this, selector, context, rootjQuerySub);
+		};
+		jQuerySub.fn.init.prototype = jQuerySub.fn;
+		var rootjQuerySub = jQuerySub(document);
+		return jQuerySub;
+	};
+
+})();
